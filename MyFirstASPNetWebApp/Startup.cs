6e1394a2a836e1910f064dff2297c5a23fb3ed00
nathanchandler26 <nathanchandler26@gmail.com>
2022-01-07 11:21:08 -0700

    Add project files.

---
+++ MyFirstASPNetWebApp/Startup.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MyFirstASPNetWebApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDefaultFiles(); // This line makes it so you don't have to manually type in /index.html at the end. It takes you straight to your page
+            app.UseStaticFiles(); // This allows the program to access the static files. This has to go after default files.
+        }
+    }
+}
---
+++ MyFirstASPNetWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,17 @@
+﻿////document.getElementById("btnSend").addEventListener("click", function () {
+////    alert("From: " + document.getElementById("txtFrom").value + 
+////        ", Subject: " + document.getElementById("txtSubject").value +
+////        ", Message: " + document.getElementById("txtMessage").value);
+////})
+
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeOut("slow");
+    // This is another thing that jquery does. It does a lot with images. This makes the image fade out when you click send.
+})
+
+// these two codes are essentially the same thing. The bottom one is more compact and is the jquery way to do it.
